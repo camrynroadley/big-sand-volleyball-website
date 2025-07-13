c490d27f06fdb78ed0280b05aa70d9d326b51f9b
camrynroadley <camrynroadley@gmail.com>
2025-07-12 17:34:34 -0700

    feat: adding email notifications for email subscriptions

--- src/app/api/subscription/route.ts
+++ src/app/api/subscription/route.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from "next/server";
+import { Resend } from "resend";
 import { createClient } from "@supabase/supabase-js";
 import { rateLimit } from "@/lib/rateLimiter";
 
@@ -8,7 +9,10 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
+const resend = new Resend(process.env.RESEND_API_KEY!);
+
 export async function POST(request: Request) {
+  console.log("SubscriptionRoute::Starting request...");
   const ip =
     request.headers.get("x-forwarded-for") ||
     request.headers.get("x-real-ip") ||
@@ -25,16 +29,18 @@ export async function POST(request: Request) {
 
   try {
     const { email } = await request.json();
-
     if (!email) {
+      console.log("SubscriptionRoute::No email provided...");
       return NextResponse.json({ error: "Email is required" }, { status: 400 });
     }
 
+    console.log("SubscriptionRoute::Adding email to supabase...");
     const { error } = await supabase
       .from("big_sand_mailing_list")
       .insert([{ email }]);
 
     if (error?.code === "23505") {
+      console.log("SubscriptionRoute::Error adding email...");
       return NextResponse.json(
         { error: "This email is already subscribed." },
         { status: 400 }
@@ -42,9 +48,31 @@ export async function POST(request: Request) {
     }
 
     if (error) {
+      console.log("SubscriptionRoute::Error adding email...");
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
+    // Send email after successful insert
+    try {
+      console.log("SubscriptionRoute::Sending notification email...");
+      const { error: resendError } = await resend.emails.send({
+        from: `Big Sand Volleyball <${process.env.RESEND_FROM_EMAIL!}>`,
+        to: process.env.RESEND_EMAIL!,
+        subject: "New Email Subscription",
+        html: `
+      <h2>New Email Submitted</h2>
+    `,
+      });
+
+      if (resendError) {
+        console.error("SubscriptionRoute::Resend Email Error:", resendError);
+      }
+    } catch (e: any) {
+      console.error(
+        "SubscriptionRoute::Failed to send notification email:",
+        e.message
+      );
+    }
     return NextResponse.json({ message: "Subscribed!" });
   } catch (e: any) {
     return NextResponse.json(
